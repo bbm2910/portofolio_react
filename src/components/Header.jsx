df4b00ad15c004f89fa86a6efae6134f90119361
bobby2910 <b.mastacan@gmail.com>
2025-06-05 13:15:50 +0100

    Add burger menu on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,35 @@
+import { useState } from "react";
 import "./Header.css";
 import Title from "./Title";
 
 const Header = ({ scrollToSection, aboutRef, projectsRef, cvRef }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const scrollToBottom = () => {
     window.scrollTo({
       top: document.body.scrollHeight,
       behavior: "smooth",
     });
+    setMenuOpen(false); // close menu on click
+  };
+
+  const handleNavClick = (ref) => {
+    scrollToSection(ref);
+    setMenuOpen(false); // close menu on click
   };
 
   return (
     <div className="header">
       <Title />
       <nav className="glitch-nav">
-        <div className="menu btns">
-          <button className="btn" onClick={() => scrollToSection(aboutRef)}>
+        <div className="burger" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </div>
+        <div className={`menu btns ${menuOpen ? "open" : ""}`}>
+          <button className="btn" onClick={() => handleNavClick(aboutRef)}>
             ABOUT
           </button>
-          {/* <button
-						className='btn'
-						onClick={() => scrollToSection(projectsRef)}
-					>
-						PROJECTS
-					</button> */}
-          <button className="btn" onClick={() => scrollToSection(cvRef)}>
+          <button className="btn" onClick={() => handleNavClick(cvRef)}>
             CV
           </button>
           <button className="btn" onClick={scrollToBottom}>
